38ba32123cb2c2c9e6d7bd64a003844cc0bfc7fb
Albert <38919412+abadiag@users.noreply.github.com>
2022-09-12 13:36:04 +0200

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.clapsApp.app',
+  appName: 'claps-app',
+  webDir: 'dist/claps-app',
+  bundledWebRuntime: false
+};
+
+export default config;
