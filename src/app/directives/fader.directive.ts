a00e3c17c73066109f33d49a7fe5b159ea1dc46c
Albert <38919412+abadiag@users.noreply.github.com>
2022-09-13 10:23:33 +0200

    fader mouse eventing directive

--- src/app/directives/fader.directive.ts
+++ src/app/directives/fader.directive.ts
@@ -13,35 +13,71 @@ export class FaderDirective implements OnInit {
   masXPosition: number = 60;
   minXPosition: number = -300;
 
+  isMouseDown: boolean = false;
+  isMouseHover: boolean = false;
+  
+  mouseDownPosition!: number;
+  mouseOffset: number = 0;
+
   constructor(private el: ElementRef
     // , @Inject(DOCUMENT) private document: any
     ) { }
   
   @HostListener('mousewheel', ['$event']) onMouse(e: any) {
     e.preventDefault();
-    this.moveFromMWheel(e.deltaY);
+    this.moveFaderHead(e.deltaY);
   }
 
+  @HostListener('mouseleave', ['$event']) 
+  onMouseLeave(e: MouseEvent) {
+    this.isMouseHover =  false; 
+     this.isMouseDown =  false; 
+     this.mouseDownPosition = e.clientX;
+  }
+
+  @HostListener("mouseover", ['$event']) onMouseHover(e: MouseEvent)
+  {
+    e.preventDefault();
+    this.isMouseHover =  true;    
+  };
+  @HostListener("mousedown", ['$event']) onMouseDown(e: MouseEvent)
+  {
+    e.preventDefault();
+    this.mouseOffset = 0;
+    this.mouseDownPosition = e.clientX;
+    this.isMouseDown =  true; 
+    console.log("mouse down");   
+  };
+
+   @HostListener("mouseup", ['$event']) onMouseUp(e: MouseEvent)
+  {
+    e.preventDefault();
+    this.mouseOffset = 0;
+    this.isMouseDown =  false;
+    this.mouseDownPosition = e.clientX;
+     console.log("mouse up");  
+  }; 
+  @HostListener("mousemove", ['$event']) onMouseMove(e: MouseEvent)
+  {
+    e.preventDefault();
+    if(this.isMouseDown && this.isMouseHover)
+    {
+      this.mouseOffset = e.clientX - this.mouseDownPosition ;
+      this.moveFaderHead(this.mouseOffset);
+      console.log(this.mouseOffset);
+    } 
+  };
+ 
   ngOnInit(): void {
     this.element = this.el.nativeElement as HTMLElement;
-    this.initDrag();
-  }
-
-  moveFromMWheel(dx: number) {
-
-    if (this.xPosition > this.minXPosition - dx / 10 && this.xPosition < this.masXPosition - dx / 10) {
-      this.xPosition += dx / 10;
-      
     }
-    
-    this.element.style.transform =
-          "translate3d(" + 0 + "px, " +  this.xPosition + "px, 0)";
 
-    console.log(this.xPosition)
+  moveFaderHead(dx: number) {
+
+    if (this.xPosition > this.minXPosition - dx && this.xPosition < this.masXPosition - dx) {
+      this.xPosition += dx;      
+      this.element.style.transform =
+            "translate3d(" + 0 + "px, " +  this.xPosition + "px, 0)"; 
+    }
   }
-
-
-
-initDrag(){}
-
 }
