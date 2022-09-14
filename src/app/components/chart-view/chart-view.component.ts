0cbf60c45fc5ad6a96f596d50ba11c0a94f82149
Albert <38919412+abadiag@users.noreply.github.com>
2022-09-14 15:15:43 +0200

    real time fix

--- src/app/components/chart-view/chart-view.component.ts
+++ src/app/components/chart-view/chart-view.component.ts
@@ -1,33 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { BaseChartDirective, Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart-view',
   templateUrl: './chart-view.component.html',
   styleUrls: ['./chart-view.component.css']
 })
-export class ChartViewComponent implements OnInit {
+export class ChartViewComponent implements OnInit, OnDestroy {
 
+  @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
 
-  lineChartData: ChartDataSets[] = [ { data: [5, 72, 78, 75, 7, 5,5, 72, 78, 75, 7, 75], label: 'Crude oil prices' },];
-  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'ww'];
+  title: string = "Chart title";
+  data: Array<number> = [];
+  lineChartData: ChartDataSets[] = [{ data: this.data, label: this.title },];
+  lineChartLabels: Label[] = [];
+
   lineChartOptions = {
     responsive: true,
+    elements: {
+      line: {
+        tension: 0.1
+      }
+    }
   };
+
   lineChartColors: Color[] = [
     {
       borderColor: 'black',
       backgroundColor: 'rgba(255,255,0,0.28)',
-      borderWidth: 1
+      borderWidth: 1,
+
     },
   ];
+
   lineChartLegend = true;
   lineChartPlugins = [];
   lineChartType: ChartType = 'line';
 
+  interval: any = -1;
+
   constructor() { }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  private updateSeries() {
+    console.log("new data");
+    let value = Math.random() * (1000 - 0) + 0;
+
+    this.data?.push(value);
+    this.lineChartLabels.push("");
+
+    if (this.lineChartData[0].data != null && this.lineChartLabels.length > 50) {
+      this.lineChartLabels.shift();
+      this.lineChartData[0].data.shift();
+    }
+
+    this.chart.update();
+  }
 
   ngOnInit(): void {
+    this.interval = setInterval(() => {
+      this.updateSeries();
+    }, 300);
   }
 }
